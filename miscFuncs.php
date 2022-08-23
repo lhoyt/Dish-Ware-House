722791da1c9888f12b6b630d7833b90d68585747
Lindsay Hoyt <lindsayalice.hoyt@gmail.com>
2022-08-22 22:34:08 -0400

    Update miscFuncs.php

--- miscFuncs.php
+++ miscFuncs.php
@@ -1,10 +1,11 @@
 <?php
-function redirect_user($page)
-{
-	$url= 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']);
-	$url = rtrim($url, '/\\');
-	$url.= '/' . $page;
-	header("Location: $url");
+function redirect_user( $page ) {
+	$url = 'http://' . $_SERVER[ 'HTTP_HOST' ] . dirname( $_SERVER[ 'PHP_SELF' ] );
+	$url = rtrim( $url, '/\\' );
+	$url .= '/' . $page;
+	
+	header( "Location: $url" );
+	
 	exit();
 }
 ?>
